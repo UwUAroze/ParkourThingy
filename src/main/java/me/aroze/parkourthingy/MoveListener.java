cbf3672757e2ee67d332b10f43a67be7ed783987
aroze123 <68871455+UwUAroze@users.noreply.github.com>
2022-05-29 22:26:49 +0100

    Improved upon jump generation

--- src/main/java/me/aroze/parkourthingy/MoveListener.java
+++ src/main/java/me/aroze/parkourthingy/MoveListener.java
@@ -1,6 +1,7 @@
 package me.aroze.parkourthingy;
 
 import jdk.incubator.vector.VectorOperators;
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
@@ -14,15 +15,31 @@ public class MoveListener implements Listener {
     @EventHandler
     public void onMove(PlayerMoveEvent e) {
 
-        if (!e.getPlayer().hasPermission("parkourthingy.admin")) return;
-        if (!e.getPlayer().isOnGround()) return;
+        if (!(e.getPlayer().hasPermission("parkourthingy.admin"))) return;
+        if (!(e.getPlayer().isOnGround())) return;
 
         ArrayList<Location> blocksBelow = getNonAirBlocksBelow(e.getPlayer().getLocation());
 
 
+        Bukkit.broadcastMessage("a");
+        
+        if (blocksBelow.contains(TestGenerate.parkourNextJump.get(e.getPlayer()))) {
+            Bukkit.broadcastMessage("b");
+
+            Location block = TestGenerate.parkourNextJump.get(e.getPlayer());
+
+            block.getBlock().setType(Material.DIRT);
+            TestGenerate.parkourLastJump.put(e.getPlayer(), block);
+
+            Location nextJump = block.clone().add(2,0,0);
+            TestGenerate.parkourNextJump.put(e.getPlayer(), nextJump);
+            nextJump.getBlock().setType(Material.WHITE_CONCRETE);
+
+        }
 
         for (Location block : blocksBelow) {
             if (TestGenerate.parkourNextJump.get(e.getPlayer()) == block) {
+                Bukkit.broadcastMessage("b");
 
                 block.getBlock().setType(Material.DIRT);
                 TestGenerate.parkourLastJump.put(e.getPlayer(), block);
@@ -39,7 +56,7 @@ public class MoveListener implements Listener {
 
     public ArrayList<Location> getBlocksBelow(Location loc) {
 
-        ArrayList<Location> blocksBelow = new ArrayList<Location>();
+        ArrayList<Location> blocksBelow = new ArrayList<>();
 
         Location blockBelow = loc.add(0, -1, 0);
 
@@ -61,12 +78,15 @@ public class MoveListener implements Listener {
     public ArrayList<Location> getNonAirBlocksBelow(Location loc) {
 
         ArrayList<Location> blocksBelow = getBlocksBelow(loc);
+        ArrayList<Location> valuesToRemove = new ArrayList<>();
+
         for (Location block : blocksBelow) {
-            if (block.getBlock().getType().equals(org.bukkit.Material.AIR)) {
-                blocksBelow.remove(block);
+            if (block.getBlock().getType() == org.bukkit.Material.AIR) {
+                valuesToRemove.add(block);
             }
         }
 
+        blocksBelow.removeAll(valuesToRemove);
         return blocksBelow;
     }
 
