20ac8cfdd2bc06d035949a32e3c7158f23cb0cd1
aroze123 <68871455+UwUAroze@users.noreply.github.com>
2022-05-29 20:58:48 +0100

    Skidded some methods from my other plugins

--- src/main/java/me/aroze/parkourthingy/ParkourThingy.java
+++ src/main/java/me/aroze/parkourthingy/ParkourThingy.java
@@ -1,5 +1,7 @@
 package me.aroze.parkourthingy;
 
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class ParkourThingy extends JavaPlugin {
@@ -14,4 +16,14 @@ public final class ParkourThingy extends JavaPlugin {
     public void onDisable() {
         // Plugin shutdown logic
     }
+
+    private void addCommand(String name, CommandExecutor executor) {
+        getCommand(name).setExecutor(executor);
+    }
+
+    private void addListener(Listener listener) {
+        getServer().getPluginManager().registerEvents(listener, this);
+    }
+
+
 }
