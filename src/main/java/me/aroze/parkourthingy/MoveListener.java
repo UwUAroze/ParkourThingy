1a27c1f336216bd1b7c012147e5bf2d061ecd51a
aroze123 <68871455+UwUAroze@users.noreply.github.com>
2022-05-29 21:24:51 +0100

    Seperated getBlocksBelow() method

--- src/main/java/me/aroze/parkourthingy/MoveListener.java
+++ src/main/java/me/aroze/parkourthingy/MoveListener.java
@@ -14,26 +14,32 @@ public class MoveListener implements Listener {
 
         if (e.getPlayer().hasPermission("parkourthingy.admin")) {
 
-            ArrayList<Location> blocksBelow = new ArrayList<Location>();
-
-            Location blockBelow = e.getPlayer().getLocation().add(0, -1, 0);
-
-            blocksBelow.add(blockBelow);
-            blocksBelow.add(blockBelow.clone().add(0,0,1));
-            blocksBelow.add(blockBelow.clone().add(0,0,-1));
-
-            blocksBelow.add(blockBelow.clone().add(1,0,1));
-            blocksBelow.add(blockBelow.clone().add(1,0,-1));
-            blocksBelow.add(blockBelow.clone().add(1,0,0));
-
-            blocksBelow.add(blockBelow.clone().add(-1,0,1));
-            blocksBelow.add(blockBelow.clone().add(-1,0,-1));
-            blocksBelow.add(blockBelow.clone().add(-1,0,0));
-
-
+            ArrayList<Location> blocksBelow = getBlocksBelow(e.getPlayer().getLocation());
 
         }
 
     }
+
+    public ArrayList<Location> getBlocksBelow(Location loc) {
+
+        ArrayList<Location> blocksBelow = new ArrayList<Location>();
+
+        Location blockBelow = loc.add(0, -1, 0);
+
+        blocksBelow.add(blockBelow);
+        blocksBelow.add(blockBelow.clone().add(0,0,1));
+        blocksBelow.add(blockBelow.clone().add(0,0,-1));
+
+        blocksBelow.add(blockBelow.clone().add(1,0,1));
+        blocksBelow.add(blockBelow.clone().add(1,0,-1));
+        blocksBelow.add(blockBelow.clone().add(1,0,0));
+
+        blocksBelow.add(blockBelow.clone().add(-1,0,1));
+        blocksBelow.add(blockBelow.clone().add(-1,0,-1));
+        blocksBelow.add(blockBelow.clone().add(-1,0,0));
+
+        return blocksBelow;
+    }
+
 
 }
