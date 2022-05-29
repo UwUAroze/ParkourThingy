b440cc120368e787e2270790335cb08520ab11a1
aroze123 <68871455+UwUAroze@users.noreply.github.com>
2022-05-29 21:34:56 +0100

    Added current/last jump logging + starting command

--- src/main/java/me/aroze/parkourthingy/TestGenerate.java
+++ src/main/java/me/aroze/parkourthingy/TestGenerate.java
@@ -1,12 +1,29 @@
 package me.aroze.parkourthingy;
 
+import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import java.util.HashMap;
 
 public class TestGenerate implements CommandExecutor {
+
+    HashMap<Player, Location> parkourLastJump = new HashMap<>();
+    HashMap<Player, Location> parkourCurrentJump = new HashMap<>();
+
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        return false;
+
+        Player player = (Player) sender;
+
+        Location startingBlock = player.getLocation().add(0, -1, 0);
+        player.sendMessage("Starting block: " + startingBlock.getBlockX() + " " + startingBlock.getBlockY() + " " + startingBlock.getBlockZ());
+
+        parkourCurrentJump.put(player, startingBlock);
+
+
+        return true;
     }
 }
