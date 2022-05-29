d9a79ebbd04130572c53e88c401e8e821fcf51bb
aroze123 <68871455+UwUAroze@users.noreply.github.com>
2022-05-29 19:51:45 +0100

    Initial commit

---
+++ src/main/java/me/aroze/parkourthingy/ParkourThingy.java
@@ -0,0 +1,17 @@
+package me.aroze.parkourthingy;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ParkourThingy extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
